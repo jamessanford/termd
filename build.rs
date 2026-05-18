89aa6e6c5ecf25f9a155b8df75ba25a2d8764b6c
James Sanford <jsanfordgit@froop.com>
2026-05-18 19:39:51 +0000

    build: probe for libutempter and emit cfg(has_utempter)

--- build.rs
+++ build.rs
@@ -3,5 +3,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_server(true)
         .build_client(true)
         .compile_protos(&["proto/terminal.proto"], &["proto"])?;
+
+    if pkg_config::probe_library("libutempter").is_ok() {
+        println!("cargo:rustc-cfg=has_utempter");
+    }
+
     Ok(())
 }
